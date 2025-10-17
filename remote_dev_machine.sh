2b93895d12e51167634f837f80cee03ffb78b28d
samsja <55492238+samsja@users.noreply.github.com>
2025-10-17 18:01:16 +0200

    Update remote_dev_machine.sh

--- remote_dev_machine.sh
+++ remote_dev_machine.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env bash
-
 # Colors for output
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 log_info() {
     echo -e "${GREEN}[INFO]${NC} $1"
 }
 
+log_warn() {
+    echo -e "${YELLOW}[WARN]${NC} $1"
+}
+
+# Detect if running as root
+if [ "$EUID" -eq 0 ]; then
+    log_warn "Running as root - sudo will be omitted from commands"
+    SUDO=""
+else
+    log_info "Running as non-root user - using sudo"
+    SUDO="sudo"
+fi
+
 main() {
-    # Install sudo if not present
+    # Install sudo if not present (only matters if root)
     if ! command -v sudo &> /dev/null; then
         apt update && apt install -y sudo
     fi
-
-    log_info "Installing apt packages..."
     
-    sudo apt update && sudo apt install -y git nvtop tmux htop zsh neovim exa bat magic-wormhole
-
+    log_info "Installing apt packages..."
+    $SUDO apt update && $SUDO apt install -y git nvtop tmux htop zsh neovim exa bat magic-wormhole
+    
     log_info "Installing locales..."
-    sudo apt install -y --no-install-recommends locales
-    locale-gen "en_US.UTF-8"
-
+    $SUDO apt install -y --no-install-recommends locales
+    $SUDO locale-gen "en_US.UTF-8"
+    
     log_info "Installing uv..."
     curl -LsSf https://astral.sh/uv/install.sh | sh
     
@@ -29,27 +41,24 @@ main() {
     if ! command -v uv &> /dev/null; then
         source $HOME/.local/bin/env
     fi
-
+    
     log_info "Installing zsh and oh my zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    chsh -s $(which zsh)
-
+    $SUDO chsh -s $(which zsh) $USER
+    
     log_info "Installing starship..."
-    curl -sS https://starship.rs/install.sh | sh
-
+    curl -sS https://starship.rs/install.sh | $SUDO sh -s -- -y
+    
     log_info "Installing atuin..."
     curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
-
+    
     log_info "Updating .zshrc..."
     cat > $HOME/.zshrc << 'EOL'
 export ZSH="$HOME/.oh-my-zsh"
-
 ZSH_THEME="robbyrussell"
 plugins=(git)
-
 source $ZSH/oh-my-zsh.sh
 export LANG="en_US.UTF-8"
-
 alias ls="exa"
 alias ll="exa -lal"
 alias gs="git status"
@@ -57,24 +66,23 @@ alias gl="git pull"
 alias gp="git push"
 alias gm="git commit -s -m"
 alias ga="git add"
-
+alias sq="squeue"
+alias sb="sbatch"
+alias si="sinfo"
+alias sc="scancel"
 source $HOME/.local/bin/env
-
 eval "$(starship init zsh)"
-
 . "$HOME/.atuin/bin/env"
-
 eval "$(atuin init zsh --disable-up-arrow)"
 EOL
-
+    
     log_info "Setting up git aliases..."
     git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-
-    log_info "adding alacritty info..."
+    
+    log_info "Adding alacritty info..."
     curl -sSL https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info | tic -x -
-
+    
     log_info "Installation completed! Please restart your shell and run 'zsh'"
-
 }
 
 main
